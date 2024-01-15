0f70b1eb9cbcbc749803ef3346427521a5724e18
fabiofcunha76 <fabiofcunha76@gmail.com>
2024-01-14 21:18:47 -0300

    Primeiro commit curso Springboot.

---
+++ src/main/kotlin/com/mercadolivro/MercadoDeLivrosApplication.kt
@@ -0,0 +1,11 @@
+package com.mercadolivro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MercadoDeLivrosApplication
+
+fun main(args: Array<String>) {
+	runApplication<MercadoDeLivrosApplication>(*args)
+}
---
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -0,0 +1,31 @@
+package com.mercadolivro.controller
+
+import ch.qos.logback.core.rolling.SizeAndTimeBasedRollingPolicy
+import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.model.CustomerModel
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("customers")
+class CustomerController {
+
+    @GetMapping()
+    fun getCustomer(): CustomerModel{
+        return CustomerModel("1","Fabio Cunha","fabio.cunha@spdm.org.br")
+    }
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody customer : PostCustomerRequest){
+        println(customer)
+    }
+
+}
