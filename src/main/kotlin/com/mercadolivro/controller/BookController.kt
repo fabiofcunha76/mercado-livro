f7f59211855061bbb566795b8a782629858759fe
fabiofcunha76 <fabiofcunha76@gmail.com>
2024-01-29 18:01:32 -0300

    Início da criação das classes BookController e PostBookRequest.

---
+++ src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -0,0 +1,19 @@
+package com.mercadolivro.controller
+
+import com.mercadolivro.controller.request.PostBookRequest
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("book")
+class BookController {
+
+
+    fun create( request : PostBookRequest){
+
+
+    }
+
+
+
+}
